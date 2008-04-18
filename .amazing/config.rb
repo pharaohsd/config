9c2d6f458966ccbf8ee907a2dd901f31c3f28f1d
gigamo <gigamo@gmail.com>
2008-04-18 15:23:51 +0200

    Amazing config updates again

--- .amazing/config.rb
+++ .amazing/config.rb
@@ -3,15 +3,10 @@
 import "../.passwords.rb" # GMAIL_PWD in there
 
 awesome {
-
   set :statusbar => "top"
-
   widget("battery") {
-
     set :interval => 25
-    
     property("text") {
-      
       case @state
       when :charged
         " =#{@percentage.to_i}= "
@@ -20,26 +15,19 @@ awesome {
       when :discharging
         " v#{@percentage.to_i}v "
       end
-
     }
-
     property("fg") {
-
       if @percentage.to_i <= 25
         "#ff5656"
       else
         "#a0a0a0"
       end
-
     }
   }
 
   widget("mpd") {
-
     set :interval => 1
-
     property("text") {
-
       case @state
       when :playing
         " >>: #@artist - #@title (#@position/#@length) "
@@ -48,107 +36,76 @@ awesome {
       when :stopped
         " []: not playing "
       end
-
     }
   }
 
   widget("mail") {
-
     set :module => :gmail
     set :interval => 5.minutes
     set :username => "gigamo"
     set :password => GMAIL_PWD
-
     property("text") {
-
       " #@count"
-
     }
-
     property("fg") {
-
       if @count > 0
         "#ff5656"
       else
         "#ffffff"
       end
-
     }
   }
 
   widget("pacman") {
-
     set :interval => 1.hours
-
     property("text") {
-
       " #@count "
-
     }
-
     property("fg") {
-
       if @count > 0
         "#ff5656"
       else
         "#ffffff"
       end
-
     }
   }
 
   widget("clock") {
-
     set :interval => 1
     set :format => " %H:%M:%S %d.%m.%Y "
-
   }
 
   widget("cpu_usage") {
-
     set :interval => 3
-
     property("text") {
-
       " #{@usage[1].to_i}%/#{@usage[2].to_i}% "
-
     }
-
     property("fg") {
-
       if @usage[0] >= 75
         "#ff5656"
       else
         "#ffffff"
       end
-
     }
   }
 
   widget("cpu_freq") {
-
     set :module => :cpu_info
     set :interval => 3
-
     property ("text") {
-
       if @speed[0] >= 1000
         ghz = @speed[0] / 1000
         "@ %3.2fGHz " % ghz
       else
         "@ #{@speed[0].to_i}MHz "
       end
-
     }
-
     property("fg") {
-
       if @speed[0] >= 1000
         "#ff5656"
       else
         "#a0a0a0"
       end
-
     }
   }
 }
