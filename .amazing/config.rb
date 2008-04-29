1c6e0732027ab0757a768dff867a55d5d9da7ecb
gigamo <gigamo@gmail.com>
2008-04-29 10:10:44 +0200

    Ditched blink

--- .amazing/config.rb
+++ .amazing/config.rb
@@ -1,4 +1,4 @@
-# Gigamo <gigamo@gmail.com> (28/04/08)
+# Gigamo <gigamo@gmail.com> (29/04/08)
 #
 # Configuration file for amazing (http://github.com/dag/amazing)
 # Only works with amazing's 'config' branch and awesome's awesome-3 branch.
@@ -25,14 +25,19 @@ awesome {
     set :interval => 10
 
     property("text") {
-      case @state 
-        when :charging : dir = "^"
-        when :discharging : dir = "v"
-        else dir = "="
+      dir = ""
+      if @state == :charging
+        dir = "^"
+      elsif @state == :discharging
+        dir = "v"
+      else
+        dir = "="
       end
 
-      if @percentage <= 20 : urgent(" #{dir}#{@percentage.to_i}#{dir} ")
-      else normal(" #{dir}#{@percentage.to_i}#{dir} ")
+      if @percentage <= 20
+        urgent(" #{dir}#{@percentage.to_i}#{dir} ")
+      else
+        normal(" #{dir}#{@percentage.to_i}#{dir} ")
       end
     }
   }
@@ -56,26 +61,29 @@ awesome {
 
   widget("gmail") {
     set :interval => 5.minutes
+    #set :module => :sup
     set :username => "gigamo"
     set :password => GMAIL_PWD
 
     property("text") {
-      # Make the widget blink upon new mail!
-      BLINK[@identifier] ||= []
-      if @count > 0
-        if BLINK[@identifier].empty?
-          BLINK[@identifier] << IO.popen("#{ENV["HOME"]}/bin/blink.rb 1.0 0 top #@identifier text '#{urgent(" #@count")}' '#{normal(" #@count")}'")
-        end
-      else
-        BLINK[@identifier].each do |blinker|
-          Process.kill("SIGINT", blinker.pid)
-        end
-      end
-
-      normal(" #@count")
-#      if @count > 0 : urgent(" #@count")
-#      else normal(" #@count")
+#      # Make the widget blink upon new mail!
+#      BLINK[@identifier] ||= []
+#      if @count > 0
+#        if BLINK[@identifier].empty?
+#          BLINK[@identifier] << IO.popen("#{ENV["HOME"]}/bin/blink.rb 1.0 0 top #@identifier text '#{urgent(" #@count")}' '#{normal(" #@count")}'")
+#        end
+#      else
+#        BLINK[@identifier].each do |blinker|
+#          Process.kill("SIGINT", blinker.pid)
+#        end
 #      end
+#
+#      normal(" #@count")
+      if @count > 0
+        urgent(" #@count")
+      else
+        normal(" #@count")
+      end
     }
   }
 
@@ -83,8 +91,10 @@ awesome {
     set :interval => 1.hours
 
     property("text") { 
-      if @count > 0 : urgent(" #@count ")
-      else normal(" #@count ")
+      if @count > 0
+        urgent(" #@count ")
+      else
+        normal(" #@count ")
       end
     }
   }
@@ -98,8 +108,10 @@ awesome {
     set :interval => 2
 
     property("text") { 
-      if @usage[0].to_i >= 50 : urgent(" #{@usage[1].to_i}%/#{@usage[2].to_i}% ")
-      else white(" #{@usage[1].to_i}%/#{@usage[2].to_i}% ")
+      if @usage[0].to_i >= 50
+        urgent(" #{@usage[1].to_i}%/#{@usage[2].to_i}% ")
+      else
+        white(" #{@usage[1].to_i}%/#{@usage[2].to_i}% ")
       end
     }
   }
@@ -109,10 +121,11 @@ awesome {
     set :interval => 3
 
     property ("text") {
+      ghz = @speed[0] / 1000
       if @speed[0] >= 1000
-        ghz = @speed[0] / 1000
-          urgent("@ %3.2fGHz " % ghz)
-      else normal("@ #{@speed[0].to_i}MHz ")
+        urgent("@ %3.2fGHz " % ghz)
+      else 
+        normal("@ #{@speed[0].to_i}MHz ")
       end
     }
   }
